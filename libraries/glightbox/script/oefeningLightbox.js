d12383c3bd5abf888e50241217f63c5b9d507c0d
TBroucke <55688055+TBroucke@users.noreply.github.com>
2021-03-09 12:01:32 +0100

    Add files via upload

---
+++ libraries/glightbox/script/oefeningLightbox.js
@@ -0,0 +1,40 @@
+"use strict";
+
+//oefening 2:
+//images array voor oefening 2
+
+//oefening 3
+//images array voor oefening 3
+
+const OefeningTwee = function() {
+//variabele aanmaken die bijhoudt waar de divs moeten komen
+
+// array overlopen en div aanmaken per image
+
+// lightbox initialiseren op basis van klassenaam
+};
+
+const OefeningDrie = function() {
+  //oefening 3
+//variabele aanmaken die bijhoudt waar de divs moeten komen
+
+// array overlopen en div aanmaken per image
+
+// lightbox initialiseren op basis van klassenaam, slidefeffect op 'fade' zetten
+
+
+};
+
+const init = function() {
+  //oefening 1: GLightbox initialiseren
+ 
+
+  //oefening 2
+  //controleren of ".c-destination__one" en ".c-destination__two" bestaan, en dan oefeningTwee oproepen
+
+//oefening 3
+   //controleren of ".c-destination__three" en dan oefeningDrie oproepen
+ 
+};
+
+document.addEventListener("DOMContentLoaded", init);
